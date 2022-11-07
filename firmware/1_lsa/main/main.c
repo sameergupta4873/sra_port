e67dbbbed75fe23b8596f9c0bcf09da8dd04d9ab
PritK99 <pritpjk@gmail.com>
2022-11-07 15:51:40 +0530

    Testing LSA

--- firmware/1_lsa/main/main.c
+++ firmware/1_lsa/main/main.c
@@ -1,4 +1,4 @@
-#include "jagura.h"
+#include "jaguar.h"
 //this code will print the readings of LSA on terminal
 
 void app_main(void)
@@ -8,5 +8,6 @@ void app_main(void)
     while (1)
     {
         get_raw_lsa() ; //prints the value of readings 
+        vTaskDelay(100 / portTICK_PERIOD_MS); 
     }
 }
